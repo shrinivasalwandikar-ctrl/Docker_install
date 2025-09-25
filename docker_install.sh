16c90f4007fb3554db03d75dcea1f5a2b177d5f3
shrinivasalwandikar-ctrl <shrinivasalwandikar@gmail.com>
2025-09-25 15:41:24 +0530

    Update docker_install.sh

--- docker_install.sh
+++ docker_install.sh
@@ -14,4 +14,8 @@ echo \
 sudo apt-get update
 
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo usermod -aG docker $USER
+newgrp docker
+docker ps
+
 
